cdf577e96b65156505f5fdf8363c2a8e2e2bbc0a
XioXzEz <xioxzzzzz@gmail.com>
2025-03-20 21:19:05 +0300

    Add files via upload

---
+++ exercise1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Процентная ставка банка равна 7%/мес.");
+        Console.Write("Введите сумму вклада: ");
+        double summ = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Введите количество месяцев: ");
+        double month = Convert.ToDouble(Console.ReadLine());
+        double result = summ * month * 0.07;
+        Console.WriteLine(result);
+    }
+}
