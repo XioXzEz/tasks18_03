e0f36ced2f188c7fead1ac62f779928cde468954
XioXzEz <xioxzzzzz@gmail.com>
2025-03-20 21:44:58 +0300

    Add files via upload

---
+++ exercise2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        decimal procent = 0.07m;
+        int i = 0;
+        Console.WriteLine("Процентная ставка банка равна 7%/мес.");
+        Console.Write("Введите сумму вклада: ");
+        decimal summ = Convert.ToDecimal(Console.ReadLine());
+        Console.Write("Введите количество месяцев: ");
+        int month = Convert.ToInt32(Console.ReadLine());
+        decimal res = summ;
+        while (i < month) 
+        {
+            res += res * procent;
+            i++;
+        }
+        Console.WriteLine($"Итоговая сумма равна: {res:F2}");
+    }
+}
